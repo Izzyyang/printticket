6c2c7007d754c0695e426d6a14b3dd6335e904dc
liadbiz <zhuhaihao_hust@firefox.com>
2016-07-17 19:39:14 +0800

    [#18 liadbiz] add 2 json files for testing and modify json file -- buy_two_getonefree

---
+++ src/test/java/TestTwoGetOneFreeDiscount.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestTwoGetOneFreeDiscount {
+//    @Test
+//    public void should_return_2_0_when_getcarts_two_getonefree() throws Exception {
+//        String type = "discount_twofreeone";
+//        assertEquals(2.0, new Cart(type).account(), 0.001);
+//    }
+
+}
